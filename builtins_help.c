3a74135d4b3cf911340c21126925e5f01cd0e63d
mariagirges <mariagirges07@gmail.com>
2023-10-11 14:11:51 -0700

    simple shell

--- builtins_help.c
+++ builtins_help.c
@@ -5,13 +5,21 @@
  */
 void shell_information(void)
 {
-	_putchar("\nShell Version 1.0.0\n\n");
-	_putchar("Usage: ./hsh\n\n");
-	_putchar("Shell built-in commands:\n\n");
-	_putchar("help\t\tDisplay this help information\n\n");
-	_putchar("cd [dir]\tChange the current working directory\n\n");
-	_putchar("env\t\tDisplay the environment variables\n\n");
-	_putchar("setenv\t\tSet an environment variable\n\n");
-	_putchar("unsetenv\tUnset an environment variable\n\n");
-	_putchar("exit\t\tExit the shell\n\n");
+	_puts("\nShell Version 1.0.0\n\n");
+
+	_puts("Usage: ./hsh\n\n");
+
+	_puts("Shell built-in commands:\n\n");
+
+	_puts("help\t\tDisplay this help information\n\n");
+
+	_puts("cd [dir]\tChange the current working directory\n\n");
+
+	_puts("env\t\tDisplay the environment variables\n\n");
+
+	_puts("setenv\t\tSet an environment variable\n\n");
+
+	_puts("unsetenv\tUnset an environment variable\n\n");
+
+	_puts("exit\t\tExit the shell\n\n");
 }
