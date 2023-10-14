9385c2ffaa300c08ef10db41ff8f52f436c11281
mariagirges <mariagirges07@gmail.com>
2023-10-14 13:09:49 +0300

    simpleshell

---
+++ printstr.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+/**
+ * printstr - print string
+ * @str: string that printed
+ * Return: void
+ */
+void printstr(char *str)
+{
+	size_t l;
+	ssize_t numberwritten;
+
+	l = _strlen(str);
+	numberwritten = write(STDOUT_FILENO, str, l);
+	
+	if (numberwritten == -1)
+		perror("write");
+}
+
+/**
+ * printerror - prints an error message
+ * @error: error message that printed
+ * Return: void
+ */
+void printerror(char *error)
+{
+	size_t l, numberwritten;
+	
+	l = _strlen(err);
+	numberwritten = write(STDERR_FILENO, err, l);
+
+	if (numberwritten == -1)
+		perror("write");
+}
--- shell.h
+++ shell.h
@@ -13,19 +13,24 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <ctype.h>
+
 /*macros*/
 #define PATH_MAX_LENGTH 4096
 #define PATH_SEPARATOR ":"
 #define PROMPT "$ "
 #define MAX_TOKENS 1024
 #define BUFFER_SIZE 1024
+
 /* prompt.c */
 void prompt(void);
+
 /* get_input.c */
 char *get_input(void);
 void free_last_input(void);
+
 /* get_in.c*/
 void *get_in(void);
+
 /* built-in funcs */
 int check_builtin(char **args);
 int execute_buitlin(char *cmd, char **args);
@@ -36,40 +41,51 @@ int setenv_shell(char **args);
 int unsetenv_shell(char **args);
 int shell_environ(void);
 int clear(char **args);
+
 /* signal_handler.c */
 void handle_sigint(int sig);
 void handle_sigquit(int sig);
 void handle_sigstp(int sig);
+
 /* execute.c */
 int execute(char **args);
+
 /* parser.c */
 char **tokenize(char *str, const char *delim);
 char **tokenize_input(char *input);
+
 /* get_env.c */
 char *_getenv(const char *name);
+
 /* get_path.c */
 char *get_path(void);
+
 /* find_in_path.c */
 char *find_in_path(char *command);
+
 /* free.c */
 void free_error(char **argv, char *arg);
 void free_tokens(char **ptr);
 void free_path(void);
-/* error.c */
-void _puts(char *str);
-void _puterror(char *err);
+
+/* printstr.c */
+void printstr(char *str);
+void printerror(char *error);
+
 /* utils_funcs1.c */
 int _strlen(const char *);
 int _strcmp(const char *s1, const char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 char *_strstr(char *haystack, char *needle);
 char *_strchr(char *s, char c);
+
 /* utils_funcs2.c */
 char *_strcpy(char *, char *);
 char *_strcat(char *, const char *);
 char *_strdup(const char *);
 int _putchar(char);
 unsigned int _strspn(char *s, char *accept);
+
 /* utils_funcs3.c */
 int _atoi(const char *str);
 char *_memset(char *, char, unsigned int);
