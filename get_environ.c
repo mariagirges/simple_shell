7ff75c10672d04a4ae2c1eaa88ef94275272b694
mariagirges <mariagirges07@gmail.com>
2023-10-16 22:54:40 +0300

    simple shell

--- get_environ.c
+++ get_environ.c
@@ -2,13 +2,13 @@
 /**
  * get_environ - find the value of environment
  * @thename: name of environment
- * @Return: the value of environment
+ * Return: the value of environment
  */
 char *get_environ(const char *thename)
 {
 	char **environ;
 	size_t name;
-	
+
 	name = _strlen(thename);
 
 	for (environ = environ; *environ != NULL; environ++)
